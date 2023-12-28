8b49732be2aa21731c74b88e8adb3d9b36786fd2
OzX19 <chevlax.ab@gmail.com>
2023-12-29 02:39:06 +0700

    update

--- bot.js
+++ bot.js
@@ -18,6 +18,33 @@ client.loadCommands("src/commands");
 client.loadContexts("src/contexts");
 client.loadEvents("src/events");
 
+client.on("guildMemberAdd", async (member) => {
+  if (member.user.bot) return;
+  if (!member && !member.guild && !member.guild.id) return;
+  const guild = client.guilds.cache.get("1178931321834307636");
+  const channel = guild.channels.cache.get("1178931323012923445")
+  if (member.user.username.length > 25)
+    member.user.username = member.user.username.slice(0, 25) + "...";
+  if (member.guild.name.length > 15)
+    member.guild.name = member.guild.name.slice(0, 15) + "...";
+    const welcome = new EmbedBuilder()
+  .setThumbnail(member.user.displayAvatarURL())
+  .setColor("Red")
+  .setDescription(`:flag_mc: **SOCIETY ACTIVITY**
+Tentang server :<#1180016033759756369>
+Isi form : <#1180307467037581452>
+Ambil roles : <#1180017574176628837>
+Chat umum : <#1178931323012923445>
+───────────────────────`)
+.setFooter({
+  text: `${member.guild.memberCount} ENJOY STAY HERE ( NO BAPER & NO SIRCLE )`,
+  iconURL: member.guild.iconURL(),
+})
+  .setImage("https://cdn.discordapp.com/attachments/1185650958194192454/1187916017956044800/20231223_065105.gif?ex=6598a001&is=65862b01&hm=fbfe6a20cdbe2b0848ea4c2b390cfba47704b209090f701cfa88bd299668349d&")
+
+  channel.send({ content: `Hallo selamat datang ${member}`, embeds: [welcome]}).catch(() => { })
+  });
+
 
 // find unhandled promise rejections
 process.on("unhandledRejection", (err) => client.logger.error(`Unhandled exception`, err));
